664fb217d76ec1c5d26e0ed8bb4ad1aa8389b9bb
Charlie-aac <63747875+Charlie-aac@users.noreply.github.com>
2020-05-27 23:45:36 -0500

    Add files via upload

---
+++ tokens.h
@@ -0,0 +1,59 @@
+/*Fecha: 24/05/2020
+Autor: KMBP
+Descripcion: Se definenen los valores para las reglas del analisis lexico */
+//valores que reconoce el lenguajes
+#define INT 	1	
+#define FLOAT	2
+#define DOUBLE  3
+#define CHAR	4
+#define SIN	5
+#define	STRUCT	6
+#define DEF	7
+#define	IF	8
+#define	WHILE	9
+#define	DO	10
+#define RETURN 	11
+#define SWITCH  12
+#define BREAK 	13
+#define WRITE	14
+#define READ	15
+#define	CASE	16
+#define	OR	17
+#define	AND	18
+#define	NOT	19
+#define	TRUE	20
+#define	FALSE	21
+#define	LPAR	22
+#define	RPAR	23
+#define MUL	24
+#define	DIV	25
+#define	MOD	26
+#define	PLUS	27
+#define	MINUS	28
+#define	LESS	29
+#define	LESSEQ	30
+#define	MORE	31
+#define MOREEQ  32
+#define	ASIGN	33
+#define	SCOLON 	34
+#define	COMMA	35
+#define COLON 	36
+#define	POINT	37
+#define DEFAULT 38
+#define ELSE    39
+#define START	40
+#define ID	41
+/*Fecha: 25/05/2020
+Autor: CAAC
+Descripcion: Se agregan valores faltantes */
+#define ASIGN2  42
+#define END 	43
+#define NUM 44
+#define THEN 45
+#define TERM 46
+#define DCOLON 47
+#define LIB 48
+#define CADENA 49
+#define NOTEQ 50
+#define CARAC 51
+#define NREAL 52
